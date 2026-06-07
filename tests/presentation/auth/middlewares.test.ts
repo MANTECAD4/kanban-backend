76cc6e8b727b0c8dc95193df88e079635756f9e3
Daniel Martinezzz <danielmt.002@gmail.com>
2026-06-06 18:04:50 -0600

    tested auth all middlewares

--- tests/presentation/auth/middlewares.test.ts
+++ tests/presentation/auth/middlewares.test.ts
@@ -4,7 +4,9 @@ import { AuthMiddlewares } from "../../../src/presentation/auth/middlewares";
 import { Request, Response } from "express";
 import {
   AccessTokenPayload,
+  LoginUserDto,
   RefreshTokenPayload,
+  RegisterUserDto,
 } from "../../../src/application/dtos";
 import {
   CustomError,
@@ -13,215 +15,329 @@ import {
 import { JwtGenerator } from "../../../src/infraestructure/services/jwt-generator.service";
 
 describe("Auth Middlewares", () => {
-  const accessTokenDuration = 15;
-  const tokenSecret = "im-a-token-secret";
-  const refreshTokenDuration = 15;
+  test("should have login data validation, register data validation & validate access token middlewares", () => {
+    const authMiddlewares = new AuthMiddlewares({} as TokenProvider);
+    expect(authMiddlewares).toHaveProperty("loginDataValidation");
+    expect(authMiddlewares).toHaveProperty("registerDataValidation");
+    expect(authMiddlewares).toHaveProperty("validateAccessToken");
 
-  const mockAccessTokenPayload: AccessTokenPayload = {
-    sub: { id: 10 },
-    type: "access",
-  };
-
-  const mockNextFn = vi.fn();
-  const mockResponse = {} as Response;
-
-  const unauthorizedErrorSpy = vi.spyOn(CustomError, "unauthorized");
-  const handleErrorSpy = vi
-    .spyOn(CustomError, "handleError")
-    //@ts-expect-error
-    .mockImplementation(() => {});
-
-  beforeEach(() => {
-    vi.useFakeTimers();
-    vi.clearAllMocks();
+    expectTypeOf(authMiddlewares.loginDataValidation).toBeFunction();
+    expectTypeOf(authMiddlewares.registerDataValidation).toBeFunction();
+    expectTypeOf(authMiddlewares.validateAccessToken).toBeFunction();
   });
 
-  describe("Positive cases", () => {
-    test("should have login data validation, register data validation & validate access token middlewares", () => {
+  describe("Login Data Validation Middleware", () => {
+    test("should store validated data into 'validatedBody' property inside Request", () => {
       const authMiddlewares = new AuthMiddlewares({} as TokenProvider);
-      expect(authMiddlewares).toHaveProperty("loginDataValidation");
-      expect(authMiddlewares).toHaveProperty("registerDataValidation");
-      expect(authMiddlewares).toHaveProperty("validateAccessToken");
 
-      expectTypeOf(authMiddlewares.loginDataValidation).toBeFunction();
-      expectTypeOf(authMiddlewares.registerDataValidation).toBeFunction();
-      expectTypeOf(authMiddlewares.validateAccessToken).toBeFunction();
-    });
-
-    test("'validateAccessToken' should extract authorization header and validate its content with given token provider & finally call next Fn", () => {
-      const mockTokenGenerator: TokenProvider = {
-        generate: vi.fn(),
-        validate: vi.fn().mockReturnValue(mockAccessTokenPayload),
+      const mockBody: LoginUserDto = {
+        email: "test@gmail.com",
+        password: "test1234+",
       };
-      const mockToken = "this-should-be-a-token";
 
       const mockRequest = {
-        header: vi.fn().mockReturnValue(`Bearer ${mockToken}`),
-      } as unknown as Request;
+        body: mockBody,
+      } as any;
 
-      const authMiddlewares = new AuthMiddlewares(mockTokenGenerator);
+      const mockNextFn = vi.fn();
 
-      authMiddlewares.validateAccessToken(
+      authMiddlewares.loginDataValidation(
         mockRequest,
         {} as Response,
         mockNextFn,
       );
 
-      expect(mockTokenGenerator.validate).toHaveBeenCalledWith(mockToken);
       expect(mockNextFn).toHaveBeenCalled();
+      expect(mockRequest).toHaveProperty("validatedBody");
+      expect(mockRequest.validatedBody).toEqual({
+        email: expect.any(String),
+        password: expect.any(String),
+      });
+    });
+    test(`should respond with a 'bad request' error if recieved data is invalid`, () => {
+      const badRequestErrorSpy = vi.spyOn(CustomError, "badRequest");
+      const handleErrorSpy = vi
+        .spyOn(CustomError, "handleError")
+        // @ts-expect-error
+        .mockImplementation(() => {});
+      const mockRequest = {
+        body: {},
+      } as Request;
+
+      const mockReponse = {} as Response;
+
+      const mockNextFn = vi.fn();
+
+      const authMiddlewares = new AuthMiddlewares({} as TokenProvider);
+      authMiddlewares.loginDataValidation(mockRequest, mockReponse, mockNextFn);
+
+      expect(badRequestErrorSpy).toHaveBeenCalled();
+      expect(handleErrorSpy).toHaveBeenCalledWith(
+        expect.any(CustomError),
+        mockRequest,
+        mockReponse,
+      );
+
+      expect(mockNextFn).not.toHaveBeenCalled();
     });
   });
-  describe("Negative cases", () => {
-    test(`Validate Access token should respond with an error if 'authorization' header is not included`, () => {
+  describe("Register Data Validation Middleware", () => {
+    test("should store validated data into 'validatedBody' property inside Request", () => {
       const authMiddlewares = new AuthMiddlewares({} as TokenProvider);
 
-      const mockRequest = {
-        header: vi.fn(),
+      const mockBody: RegisterUserDto = {
+        name: "test user",
+        email: "test@gmail.com",
+        password: "test1234+",
       };
 
-      authMiddlewares.validateAccessToken(
-        //@ts-expect-error
+      const mockRequest = {
+        body: mockBody,
+      } as any;
+
+      const mockNextFn = vi.fn();
+
+      authMiddlewares.registerDataValidation(
         mockRequest,
-        mockResponse,
+        {} as Response,
         mockNextFn,
       );
 
-      expect(unauthorizedErrorSpy).toHaveBeenCalledWith(
-        "No token provided",
-        ErrorCodes.UNAUTHORIZED,
-      );
-
-      expect(handleErrorSpy).toHaveBeenCalledWith(
-        expect.any(CustomError),
-        mockRequest,
-        mockResponse,
-      );
-
-      expect(mockNextFn).not.toHaveBeenCalled();
+      expect(mockNextFn).toHaveBeenCalled();
+      expect(mockRequest).toHaveProperty("validatedBody");
+      expect(mockRequest.validatedBody).toEqual(mockBody);
     });
+    test(`should respond with a 'bad request' error if recieved data is invalid`, () => {
+      const badRequestErrorSpy = vi.spyOn(CustomError, "badRequest");
+      const handleErrorSpy = vi
+        .spyOn(CustomError, "handleError")
+        // @ts-expect-error
+        .mockImplementation(() => {});
+      const mockRequest = {
+        body: {},
+      } as Request;
 
-    test(`Validate access token should respond with an error if authorization header doens't start with 'Bearer ', like a Bearer token should do `, () => {
+      const mockReponse = {} as Response;
+
+      const mockNextFn = vi.fn();
+
       const authMiddlewares = new AuthMiddlewares({} as TokenProvider);
-      const mockRequest = {
-        header: vi.fn().mockReturnValue("not-a-bearer-token"),
-      } as unknown as Request;
-
-      authMiddlewares.validateAccessToken(
+      authMiddlewares.registerDataValidation(
         mockRequest,
-        mockResponse as Response,
+        mockReponse,
         mockNextFn,
       );
 
-      expect(unauthorizedErrorSpy).toHaveBeenCalledWith(
-        "Invalid token",
-        ErrorCodes.INVALID_TOKEN,
-      );
-
+      expect(badRequestErrorSpy).toHaveBeenCalled();
       expect(handleErrorSpy).toHaveBeenCalledWith(
         expect.any(CustomError),
         mockRequest,
-        mockResponse,
+        mockReponse,
       );
 
       expect(mockNextFn).not.toHaveBeenCalled();
     });
+  });
 
-    test("Validate access should respond with an error if recieved token is invalid (corrupted, malformed)", () => {
-      const jwtProvider = new JwtGenerator(tokenSecret);
-      const authMiddlewares = new AuthMiddlewares(jwtProvider);
+  describe("Validation Access Token Middleware", () => {
+    const accessTokenDuration = 15;
+    const tokenSecret = "im-a-token-secret";
+    const refreshTokenDuration = 15;
 
-      const invalidToken = "this-is-an-invalid-token";
-      const mockRequest = {
-        header: vi.fn().mockReturnValue(`Bearer ${invalidToken}`),
-      } as unknown as Request;
-      const mockResponse = {} as Response;
+    const mockAccessTokenPayload: AccessTokenPayload = {
+      sub: { id: 10 },
+      type: "access",
+    };
 
-      authMiddlewares.validateAccessToken(
-        mockRequest,
-        mockResponse,
-        mockNextFn,
-      );
+    const mockNextFn = vi.fn();
+    const mockResponse = {} as Response;
 
-      expect(unauthorizedErrorSpy).toHaveBeenCalledWith(
-        expect.any(String),
-        ErrorCodes.INVALID_TOKEN,
-      );
-      expect(handleErrorSpy).toHaveBeenCalledWith(
-        expect.any(CustomError),
-        mockRequest,
-        mockResponse,
-      );
-      expect(mockNextFn).not.toHaveBeenCalled();
+    const unauthorizedErrorSpy = vi.spyOn(CustomError, "unauthorized");
+    const handleErrorSpy = vi
+      .spyOn(CustomError, "handleError")
+      //@ts-expect-error
+      .mockImplementation(() => {});
+
+    beforeEach(() => {
+      vi.useFakeTimers();
+      vi.clearAllMocks();
     });
+    describe("Positive cases", () => {
+      test("'validateAccessToken' should extract authorization header and validate its content with given token provider & finally call next Fn", () => {
+        const mockTokenGenerator: TokenProvider = {
+          generate: vi.fn(),
+          validate: vi.fn().mockReturnValue(mockAccessTokenPayload),
+        };
+        const mockToken = "this-should-be-a-token";
 
-    test("Valuidate access token should respond with an error if an expired token is recieved", () => {
-      const jwtProvider = new JwtGenerator(tokenSecret);
-      const authMiddlewares = new AuthMiddlewares(jwtProvider);
+        const mockRequest = {
+          header: vi.fn().mockReturnValue(`Bearer ${mockToken}`),
+        } as unknown as Request;
 
-      const expiredAccessToken = jwtProvider.generate(
-        mockAccessTokenPayload,
-        accessTokenDuration,
-      );
+        const authMiddlewares = new AuthMiddlewares(mockTokenGenerator);
 
-      const mockRequest = {
-        header: vi.fn().mockReturnValue(`Bearer ${expiredAccessToken}`),
-      } as unknown as Request;
-      const mockResponse = {} as Response;
-      vi.advanceTimersByTime(1000 * 60 * (accessTokenDuration + 1));
+        authMiddlewares.validateAccessToken(
+          mockRequest,
+          {} as Response,
+          mockNextFn,
+        );
 
-      authMiddlewares.validateAccessToken(
-        mockRequest,
-        mockResponse,
-        mockNextFn,
-      );
-
-      expect(unauthorizedErrorSpy).toHaveBeenCalledWith(
-        expect.stringMatching(/expired/i),
-        ErrorCodes.INVALID_TOKEN,
-      );
-      expect(handleErrorSpy).toHaveBeenCalledWith(
-        expect.any(CustomError),
-        mockRequest,
-        mockResponse,
-      );
-      expect(mockNextFn).not.toHaveBeenCalled();
+        expect(mockTokenGenerator.validate).toHaveBeenCalledWith(mockToken);
+        expect(mockNextFn).toHaveBeenCalled();
+      });
     });
+    describe("Negative cases", () => {
+      test(`Validate Access token should respond with an error if 'authorization' header is not included`, () => {
+        const authMiddlewares = new AuthMiddlewares({} as TokenProvider);
 
-    test("Validate access token should throw an error if a valid token with wrong type is recieved", () => {
-      const jwtProvider = new JwtGenerator("im-a-token-secret");
-      const authMiddlewares = new AuthMiddlewares(jwtProvider);
-      const refreshTokenPayload: RefreshTokenPayload = {
-        sub: { id: 1 },
-        type: "refresh",
-        jti: crypto.randomUUID(),
-      };
-      const refreshToken = jwtProvider.generate(
-        refreshTokenPayload,
-        refreshTokenDuration,
-      );
+        const mockRequest = {
+          header: vi.fn(),
+        };
 
-      const mockRequest = {
-        header: vi.fn().mockReturnValue(`Bearer ${refreshToken}`),
-      } as unknown as Request;
+        authMiddlewares.validateAccessToken(
+          //@ts-expect-error
+          mockRequest,
+          mockResponse,
+          mockNextFn,
+        );
 
-      authMiddlewares.validateAccessToken(
-        mockRequest,
-        mockResponse,
-        mockNextFn,
-      );
+        expect(unauthorizedErrorSpy).toHaveBeenCalledWith(
+          "No token provided",
+          ErrorCodes.UNAUTHORIZED,
+        );
 
-      expect(unauthorizedErrorSpy).toHaveBeenCalledWith(
-        "Invalid token payload",
-        ErrorCodes.INVALID_TOKEN,
-      );
+        expect(handleErrorSpy).toHaveBeenCalledWith(
+          expect.any(CustomError),
+          mockRequest,
+          mockResponse,
+        );
 
-      expect(handleErrorSpy).toHaveBeenCalledWith(
-        expect.any(CustomError),
-        mockRequest,
-        mockResponse,
-      );
+        expect(mockNextFn).not.toHaveBeenCalled();
+      });
 
-      expect(mockNextFn).not.toHaveBeenCalled();
+      test(`Validate access token should respond with an error if authorization header doens't start with 'Bearer ', like a Bearer token should do `, () => {
+        const authMiddlewares = new AuthMiddlewares({} as TokenProvider);
+        const mockRequest = {
+          header: vi.fn().mockReturnValue("not-a-bearer-token"),
+        } as unknown as Request;
+
+        authMiddlewares.validateAccessToken(
+          mockRequest,
+          mockResponse as Response,
+          mockNextFn,
+        );
+
+        expect(unauthorizedErrorSpy).toHaveBeenCalledWith(
+          "Invalid token",
+          ErrorCodes.INVALID_TOKEN,
+        );
+
+        expect(handleErrorSpy).toHaveBeenCalledWith(
+          expect.any(CustomError),
+          mockRequest,
+          mockResponse,
+        );
+
+        expect(mockNextFn).not.toHaveBeenCalled();
+      });
+
+      test("Validate access should respond with an error if recieved token is invalid (corrupted, malformed)", () => {
+        const jwtProvider = new JwtGenerator(tokenSecret);
+        const authMiddlewares = new AuthMiddlewares(jwtProvider);
+
+        const invalidToken = "this-is-an-invalid-token";
+        const mockRequest = {
+          header: vi.fn().mockReturnValue(`Bearer ${invalidToken}`),
+        } as unknown as Request;
+        const mockResponse = {} as Response;
+
+        authMiddlewares.validateAccessToken(
+          mockRequest,
+          mockResponse,
+          mockNextFn,
+        );
+
+        expect(unauthorizedErrorSpy).toHaveBeenCalledWith(
+          expect.any(String),
+          ErrorCodes.INVALID_TOKEN,
+        );
+        expect(handleErrorSpy).toHaveBeenCalledWith(
+          expect.any(CustomError),
+          mockRequest,
+          mockResponse,
+        );
+        expect(mockNextFn).not.toHaveBeenCalled();
+      });
+
+      test("Valuidate access token should respond with an error if an expired token is recieved", () => {
+        const jwtProvider = new JwtGenerator(tokenSecret);
+        const authMiddlewares = new AuthMiddlewares(jwtProvider);
+
+        const expiredAccessToken = jwtProvider.generate(
+          mockAccessTokenPayload,
+          accessTokenDuration,
+        );
+
+        const mockRequest = {
+          header: vi.fn().mockReturnValue(`Bearer ${expiredAccessToken}`),
+        } as unknown as Request;
+        const mockResponse = {} as Response;
+        vi.advanceTimersByTime(1000 * 60 * (accessTokenDuration + 1));
+
+        authMiddlewares.validateAccessToken(
+          mockRequest,
+          mockResponse,
+          mockNextFn,
+        );
+
+        expect(unauthorizedErrorSpy).toHaveBeenCalledWith(
+          expect.stringMatching(/expired/i),
+          ErrorCodes.INVALID_TOKEN,
+        );
+        expect(handleErrorSpy).toHaveBeenCalledWith(
+          expect.any(CustomError),
+          mockRequest,
+          mockResponse,
+        );
+        expect(mockNextFn).not.toHaveBeenCalled();
+      });
+
+      test("Validate access token should throw an error if a valid token with wrong type is recieved", () => {
+        const jwtProvider = new JwtGenerator("im-a-token-secret");
+        const authMiddlewares = new AuthMiddlewares(jwtProvider);
+        const refreshTokenPayload: RefreshTokenPayload = {
+          sub: { id: 1 },
+          type: "refresh",
+          jti: crypto.randomUUID(),
+        };
+        const refreshToken = jwtProvider.generate(
+          refreshTokenPayload,
+          refreshTokenDuration,
+        );
+
+        const mockRequest = {
+          header: vi.fn().mockReturnValue(`Bearer ${refreshToken}`),
+        } as unknown as Request;
+
+        authMiddlewares.validateAccessToken(
+          mockRequest,
+          mockResponse,
+          mockNextFn,
+        );
+
+        expect(unauthorizedErrorSpy).toHaveBeenCalledWith(
+          "Invalid token payload",
+          ErrorCodes.INVALID_TOKEN,
+        );
+
+        expect(handleErrorSpy).toHaveBeenCalledWith(
+          expect.any(CustomError),
+          mockRequest,
+          mockResponse,
+        );
+
+        expect(mockNextFn).not.toHaveBeenCalled();
+      });
     });
   });
 });
